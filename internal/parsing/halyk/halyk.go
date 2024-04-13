fe410ceb8e4ea0ee9dc3f3231b1d6f3c3de2e08c
Dikontay <dalimbayeva@icloud.com>
2024-04-14 03:15:25 +0500

    complete halyk

--- internal/parsing/halyk/halyk.go
+++ internal/parsing/halyk/halyk.go
@@ -3,12 +3,14 @@ package halyk
 import (
 	"errors"
 	"fmt"
+
 	"github.com/Dikontay/btscase/internal/models"
 	"github.com/playwright-community/playwright-go"
 	"log"
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type HalykParser struct {
@@ -47,9 +49,12 @@ func (parser *HalykParser) ParseHalyk() error {
 	}
 
 	//offers := make([]models.Offer, 0, len(urlsOfOffers)/
-	_, err = parser.GetOffersFromHalyk(urlsOfOffers)
+	offers, err := parser.GetOffersFromHalyk(urlsOfOffers)
 	if err != nil {
 		return err
+	}
+	for i := range offers {
+		fmt.Println(offers[i])
 	}
 
 	return nil
@@ -96,7 +101,8 @@ func (parser *HalykParser) GetURLsOfOffers(URL string) ([]string, error) {
 func (parser *HalykParser) GetOffersFromHalyk(URLs []string) ([]models.Offer, error) {
 	offers := make([]models.Offer, 0, len(URLs))
 
-	for i, URL := range URLs {
+	for _, URL := range URLs {
+		var offer models.Offer
 		if _, err := parser.Page.Goto(URL); err != nil {
 			log.Printf("could not navigate to URL %s: %v", URL, err)
 			continue
@@ -105,14 +111,43 @@ func (parser *HalykParser) GetOffersFromHalyk(URLs []string) ([]models.Offer, er
 			log.Printf("could not wait for page load: %v", err)
 			continue
 		}
+		title, err := parser.Page.Locator(`.text-2xl.mb-6.font-semibold`).InnerText()
 
-		conditions, err := parser.Page.Locator(`body .wrapper .wrapper-in .content._js-bvi-filter .page.page--news .container .flex.flex-wrap.-gw .w-7\/12 .border-b.border-gray-100.pb-6.mb-6.content-inner-editer .mb-10`).AllTextContents()
-		for i := range conditions {
-			_, err := extractLastDateFromText(conditions[i])
-			if err != nil {
-				return nil, err
+		if err != nil {
+			return nil, err
+		}
+		bonusAmount, err := getPercent(title)
+		if err != nil {
+			continue
+		}
+		offer.Precent = float64(bonusAmount)
+		offer.Market = title
+		offer.Bank = "Halyk"
+		conditions, err := parser.Page.Locator(`body .wrapper .wrapper-in .content._js-bvi-filter .page.page--news .container .flex.flex-wrap.-gw .w-7\/12 .border-b.border-gray-100.pb-6.mb-6.content-inner-editer .mb-10`).AllInnerTexts()
+
+		for j := range conditions {
+			if conditions[j] == "" {
+				continue
 			}
-			//fmt.Println(date)
+			date, err := extractLastDateFromText(conditions[j])
+			date = strings.TrimSpace(date)
+			if err != nil {
+				continue
+			}
+			bonusConditions, err := extractBonusConditions(conditions[j])
+			if err != nil {
+				continue
+
+			}
+			dateTime, err := time.Parse("02.01.2006", date)
+			if err != nil {
+				continue
+			}
+			limitations, err := getLimitations(conditions[j])
+
+			offer.Due = dateTime
+			offer.Condition = bonusConditions
+			offer.Limitation = limitations
 
 		}
 
@@ -120,9 +155,8 @@ func (parser *HalykParser) GetOffersFromHalyk(URLs []string) ([]models.Offer, er
 			log.Printf("could not wait for page load: %v", err)
 			continue
 		}
-		if i == 0 {
-			break
-		}
+
+		offers = append(offers, offer)
 
 	}
 
@@ -150,6 +184,36 @@ func getPercent(text string) (int, error) {
 	return 0, errors.New("cannot get percentage of offer")
 }
 
+func extractBonusConditions(text string) (string, error) {
+	// Compile the regular expression to match the bonus conditions section
+	re, err := regexp.Compile(`(?m)Условия начисления бонуса:\s*\n(.*?)(?:\n\S|$)`)
+	if err != nil {
+		return "", err
+	}
+
+	// Find the match for the bonus conditions section
+	match := re.FindStringSubmatch(text)
+	if match == nil || len(match) < 2 {
+		return "", fmt.Errorf("bonus conditions section not found")
+	}
+
+	// The first submatch contains the conditions text
+	return match[1], nil
+}
+
+//	func findMarketName(text string) (string, error) {
+//		re, err := regexp.Compile(`в магазин(е|ах) (\p{L}+)`)
+//		if err != nil {
+//			return "", err
+//		}
+//
+//		matches := re.FindStringSubmatch(text)
+//		if matches == nil || len(matches) < 2 {
+//			return "", fmt.Errorf("store name not found")
+//		}
+//
+//		return matches[1], nil
+//	}
 func extractLastDateFromText(text string) (string, error) {
 	// First, extract the date range text from the HTML using regex
 	re, err := regexp.Compile(`\b(\d{2}\.\d{2}\.\d{4})\b`)
@@ -158,6 +222,7 @@ func extractLastDateFromText(text string) (string, error) {
 	}
 
 	dates := re.FindAllString(text, -1)
+
 	if len(dates) == 0 {
 		return "", fmt.Errorf("no dates found in the string")
 	}
@@ -165,4 +230,20 @@ func extractLastDateFromText(text string) (string, error) {
 	// Return the last date from the matches.
 	lastDate := dates[len(dates)-1]
 	return lastDate, nil
+}
+
+func getLimitations(text string) (string, error) {
+	re, err := regexp.Compile(`(?m)Исключения по начислению бонусов:\s*\n(.*?)(?:\n\S|$)`)
+	if err != nil {
+		return "", err
+	}
+
+	// Find the match for the bonus conditions section
+	match := re.FindStringSubmatch(text)
+	if match == nil || len(match) < 2 {
+		return "", fmt.Errorf("bonus conditions section not found")
+	}
+
+	// The first submatch contains the conditions text
+	return match[1], nil
 }
